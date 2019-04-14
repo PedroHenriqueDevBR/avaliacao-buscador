278f0d819d595bf7cd6aa62418cec84eb3cbab1c
pedrohenriquedevbr <p.lyma18@gmail.com>
2019-04-14 17:25:30 -0300

    correção na passagem de parametro entre funções

--- web/poodle/aplicacao/meus_models.py
+++ web/poodle/aplicacao/meus_models.py
@@ -1,6 +1,7 @@
 import requests
 import requests_cache
 from bs4 import BeautifulSoup
+import time
 
 # Configurações iniciais
 requests_cache.install_cache(expire_after=600)  # atualiza o cache a cada 10 minutos
@@ -13,8 +14,11 @@ class Indexador:
         self.keywords = []  # a busca pode ser uma palavra apenas ou uma frase.
         self.matchs = []  # lista de sites encontrados com pelo menos uma keyword
 
+
     # Funcao principal
     def search(self, keyword, url, deth=0):
+        inicio = time.time()
+
         self.keywords = keyword.split()
         self.url = url if (url != '') else 'https://academico.ifpi.edu.br'
 
@@ -25,44 +29,47 @@ class Indexador:
             sites_sem_visita = self.visitar_sites(sites_sem_visita)
             camada_visitada += 1
 
+        fim = time.time()
+        print('\n\n==========================================')
+        print('Tempo de execução: {}'.format(fim - inicio))
+        print('==========================================\n\n')
+
+
     # Visita cada site
     def visitar_sites(self, sites):
         links = []
         for site in sites:
             try:
                 response = requests.get(site)
+                if response.status_code == 200:
+                    if self.adiciona_ponto(site):
+                        continue
+
+                    html = response.content
+                    soup = BeautifulSoup(html, 'html.parser')
+                    frases = soup.get_text().replace('\n', ' ').split()
+
+                    for keyword in self.keywords:
+                        keyword = keyword.lower()
+                        for palavra in frases:
+                            if keyword in palavra.lower():
+                                self.salvar_site(site, keyword, soup)        
+                    links = self.extrair_links(soup)
             except:
                 continue
 
-            if response.status_code == 200:
-                if self.adiciona_ponto(site):
-                    continue
-
-                html = response.content
-                soup = BeautifulSoup(html, 'html.parser')
-                frases = soup.find_all(text=True)
-
-                for keyword in self.keywords:
-                    keyword = keyword.lower()
-                    for frase in frases:
-                        palavras_da_frase = frase.split()
-                        for palavra in palavras_da_frase:
-                            if keyword in palavra.lower():
-                                self.salvar_site(site, keyword, html)
-
-                links = self.extrair_links(html)
         return links
 
+
     def adiciona_ponto(self, url):
-        if len(self.matchs) != 0:
+        if len(self.matchs) > 0:
             for site in self.matchs:
                 if site['link'] == url:
                     site['pontos'] += 1
                     return True
         return False
 
-    def extrair_links(self, html):
-        soup = BeautifulSoup(html, 'html.parser')
+    def extrair_links(self, soup):
         extracao_de_tag = soup.find_all('a')
         links_encontrados = []
 
@@ -73,34 +80,34 @@ class Indexador:
 
         return links_encontrados
 
-    def salvar_site(self, a_href, keyword, html):
-        soup = BeautifulSoup(html, 'html.parser')
+
+    def salvar_site(self, a_href, keyword, soup):
         frases = soup.find_all(text=True)
         title = a_href
         descricao = []
 
         try:
             title = soup.title.string
+            for frase in frases:
+                palavras_da_frase = frase.split()
+                for palavra in palavras_da_frase:
+                    if keyword in palavra.lower():
+                        index = palavras_da_frase.index(palavra)  # busca a posicao da palavra na frase
+
+                        # faz append de 5 palavras antes e depois da keyword:
+                        descricao.append(' '.join(palavras_da_frase[max(0, index - 5):min(index + 6, len(palavras_da_frase))]))
+
+            self.matchs.append(
+                {'link': a_href,
+                'titulo': title,
+                'descricao': '... '.join(descricao),
+                'pontos': 1
+                }
+            )
+
         except:
             pass
 
-        for frase in frases:
-            palavras_da_frase = frase.split()
-            for palavra in palavras_da_frase:
-                if keyword in palavra.lower():
-                    index = palavras_da_frase.index(palavra)  # busca a posicao da palavra na frase
-
-                    # faz append de 5 palavras antes e depois da keyword:
-                    descricao.append(
-                        ' '.join(palavras_da_frase[max(0, index - 5):min(index + 6, len(palavras_da_frase))]))
-
-        self.matchs.append(
-            {'link': a_href,
-             'titulo': title,
-             'descricao': '... '.join(descricao),
-             'pontos': 1
-             }
-        )
 
     def get_matchs(self):
         resultado = []
